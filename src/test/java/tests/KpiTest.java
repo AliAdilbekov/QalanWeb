3f38b03eb6c026013601f14b0c61f50cbd62ffb8
Temirlan <116719836+Temirlan228@users.noreply.github.com>
2025-06-10 13:18:31 +0500

    Update KpiTest.java
    newPupils - unt pupils

--- src/test/java/tests/KpiTest.java
+++ src/test/java/tests/KpiTest.java
@@ -14,16 +14,44 @@ public class KpiTest extends TestBase {
                 .clickKpiButton()
                 .clickPersonalStudyKpi()
                 .typeCompanyNameAndSelect("qalan.kz")
-                .enterDate("01.04.2022")
+                .enterDate("01.04.2024")
                 .verifyKpiRowDataWithLogs(
-                        "01.04.2022",
+                        "01.04.2024",
                         "12",
-                        "24",
-                        "50%",
-                        "3",
-                        "900.00 %"
+                        "25",
+                        "48%",
+                        "802",
+                        "103.12 %"
                 );
     }
+
+    @Test
+    void testNewPupilsChapter() {
+        new KpiTablePage()
+                .loginAsService("7000000000", "12345678", "test sbb")
+                .clickServiceChapter()
+                .clickKpiButton()
+                .clickNewPupils()
+                .selectKazakhLanguage()
+                .enterDateForNewPupils("01.04.2024")
+                .clickLoadButton()
+                .verifyNewPupilsData();
+    }
+
+    @Test
+    void testUntPupilsKpi() {
+        new KpiTablePage()
+                .loginAsService("7000000000", "12345678", "test sbb")
+                .clickServiceChapter()
+                .clickKpiButton()
+                .clickUntPupilsKpi()
+                .selectKazLanguage()
+                .enterDateForUnt("01.04.2024")
+                .clickLoadButton()
+                .verifyUntPupilsData();
+    }
+
+
 
 
 }
